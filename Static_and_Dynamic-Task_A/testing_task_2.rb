70028207066d66db1defb255eef1de292a5da3b8
Alison Wood <alisonwood29@gmail.com>
2018-01-07 18:33:35 +0000

    added pdf file with I.T 3-6

---
+++ Static_and_Dynamic-Task_A/testing_task_2.rb
@@ -0,0 +1,71 @@
+### Testing task 2 code:
+
+# Carry out dynamic testing on the code below.
+# Correct the errors below that you spotted in task 1.
+
+def func1(val)
+  if val == 1
+    return true
+  else
+    return false
+  end
+end
+
+p func1(1)
+p func1(3)
+
+def max(a, b)
+  if a > b
+    return a
+  else
+    return b
+  end
+end
+
+p max(3, 4)
+p max(5, 4)
+
+
+def looper
+  for i in 1..10
+  puts i
+  end
+end
+
+p looper
+
+
+failures = 0
+
+if looper == 10
+  puts "looper passed"
+else
+  puts "looper failed"
+  failures += 1
+end
+
+
+if func1(3) == false
+  puts "func1(3) passed"
+else
+  puts "func1(3) failed"
+  failures += 1
+end
+
+
+if max(100,1) == 100
+  puts "max(100,1) passed"
+else
+  puts "max(100,1) failed"
+  failrues += 1
+end
+
+
+if failures != 0
+  puts "Test Failed"
+else
+  puts "Test Passed"
+end
+
+
+p failures
