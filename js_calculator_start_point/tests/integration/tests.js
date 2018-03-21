af9c4cfb4f54cd84730a1581efca8631717e8c4e
Alison Wood <alisonwood29@gmail.com>
2018-03-21 10:03:13 +0000

    passing integration test for subtract button

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -31,13 +31,16 @@ describe('calculator functionality', function() {
     element(by.css('#operator_equals')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2');
   })
-  //
-  // it('should have a working add button', function () {
-  //   running_total = element(by.css('#running_total'));
-  //   element(by.css('#number1')).click();
-  //   element(by.css('#clear')).click();
-  //   expect(running_total.getAttribute('value')).to.eventually.equal('0');
-  // })
+
+  it('should have a working subtract button', function () {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number4')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3');
+  })
+
 
   it('should display infinity when dividing by 0', function () {
     running_total = element(by.css('#running_total'));
