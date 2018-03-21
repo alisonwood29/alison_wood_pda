b52e74586533d1e96e983ec17ef0eb5a1f9f8ecd
Alison Wood <alisonwood29@gmail.com>
2018-03-21 10:04:29 +0000

    passing integration test for multiply button

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -41,6 +41,15 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('3');
   })
 
+  it('should have a working multiply button', function () {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4');
+  })
+
 
   it('should display infinity when dividing by 0', function () {
     running_total = element(by.css('#running_total'));
